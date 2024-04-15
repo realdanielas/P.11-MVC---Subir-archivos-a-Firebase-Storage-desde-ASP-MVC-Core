19249e44f098f8826d9a280a076b9203a61ed327
realdanielas <daniela9.saavedra@gmail.com>
2024-04-15 00:18:29 -0600

    Add project files.

---
+++ P.11 MVC - Subir archivos a Firebase Storage desde ASP MVC Core/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+using Firebase.Auth;
+using Firebase.Storage;
+using Microsoft.AspNetCore.Mvc;
+using P._11_MVC___Subir_archivos_a_Firebase_Storage_desde_ASP_MVC_Core.Models;
+using System.Diagnostics;
+
+namespace P._11_MVC___Subir_archivos_a_Firebase_Storage_desde_ASP_MVC_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SubirArchivo(IFormFile archivo)
+        {
+            Stream archivoASubir = archivo.OpenReadStream();
+
+            string email = "ana.saavedra@catolica.edu.sv";
+            string clave = "";
+            string ruta = "aspstoragefile.appspot.com";
+            string api_key = "AIzaSyAv_g3zhXKb7eofQCfERUAPwTmRKVKEI9Q";
+
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(api_key));
+            var autenticarFirebase = await auth.SignInWithEmailAndPasswordAsync(email, clave);
+
+            var cancellation = new CancellationTokenSource();
+            var tokenUser = autenticarFirebase.FirebaseToken;
+
+            var tareaCargarArchivo = new FirebaseStorage(ruta,
+                                                            new FirebaseStorageOptions
+                                                            {
+                                                                AuthTokenAsyncFactory = () => Task.FromResult(tokenUser),
+                                                                ThrowOnCancel = true
+                                                            }
+                                                        ).Child("Archivos").Child(archivo.FileName).PutAsync(archivoASubir, cancellation.Token);
+            var urlArchivoCargado = await tareaCargarArchivo;
+
+            return RedirectToAction("VerImagen");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P.11 MVC - Subir archivos a Firebase Storage desde ASP MVC Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P._11_MVC___Subir_archivos_a_Firebase_Storage_desde_ASP_MVC_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
